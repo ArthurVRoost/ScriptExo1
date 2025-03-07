656410291b2bb33cf52589774766b344976ff82d
Arthur <arthur.vanroost@icloud.com>
2025-03-07 10:17:05 +0100

    structure public

---
+++ exo4.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mkdir -p public/{assets/img,css/{footer,header,main},pages,js} && touch index.html css/footer/style.css css/header/style.css css/main/style.css js/main.js img/img1.jpg img/img2.jpg
--- structure.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mkdir -p assets/img style pages && touch index.html style/style.css
